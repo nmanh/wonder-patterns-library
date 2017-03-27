2a42f9e0d2d02a9367c69441698cb1c0d9aed8a3
_ngmanh <manhnguyen@yola.vn>
2017-03-27 13:48:25 +0700

    create base pages for pattern library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './pages/Home';
+import GlobalStyles from './pages/GlobalStyles';
+import UIComponents from './pages/UIComponents';
+import CSSUtilities from './pages/CSSUtilities';
 
 class App extends Component {
   render() {
     return (
-      <div>Wonder Styleguide</div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Getting Started</Link></li>
+            <li><Link to="/global-styles">Global Styles</Link></li>
+            <li><Link to="/ui-components">UI Components</Link></li>
+            <li><Link to="/css-utilities">CSS Utilities</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/global-styles" component={GlobalStyles}/>
+          <Route path="/ui-components" component={UIComponents}/>
+          <Route path="/css-utilities" component={CSSUtilities}/>
+        </div>
+      </Router>
     );
   }
 }
