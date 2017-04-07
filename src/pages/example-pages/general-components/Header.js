846445f086d5b56e285c2c52d2e7fba47c7d8af7
_ngmanh <manhnguyen@yola.vn>
2017-04-07 18:11:58 +0700

    update

--- src/pages/example-pages/general-components/Header.js
+++ src/pages/example-pages/general-components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 export const Header = ({ openAskModal }) => (
   <header className="app-header mbxxlg">
-    <div className="app-header__main" style={{top: '38px'}}>
+    <div className="app-header__main">
 
       <div className="row column lf lf_align-middle mblg">
 
-        <div className="app-header__logo mrmd">
-          <img src="https://unsplash.it/140/50" alt="logo" />
-        </div>
+        <Link to="/example-pages/homepage" className="app-header__logo mrmd">
+          <h1 className="mb-reset">Wonder</h1>
+        </Link>
 
         <div className="row lf_full-space mrlg">
           <div className="medium-8 columns">
@@ -36,25 +37,25 @@ export const Header = ({ openAskModal }) => (
         </a>
 
         <div>
-          <a href="#" className="lf lf_align-middle txt-white">
+          <Link to="/example-pages/user-detail" className="lf lf_align-middle txt-white">
             <img src="https://unsplash.it/40" className="img-circle mrsm" alt="avatar" />
             <div>
               <span className="mrsm">Linh duong</span>
               <i className="fa fa-angle-down"></i>
             </div>
-          </a>
+          </Link>
         </div>
 
       </div>
 
       <div className="row column">
         <nav className="nav">
-          <a href="#" className="nav__link active">
+          <NavLink to="/example-pages/homepage" className="nav__link">
             <i className="fa fa-home" /> <span className="hide-for-small">Trang chủ</span>
-          </a>
-          <a href="#" className="nav__link">
+          </NavLink>
+          <NavLink to="/example-pages/blog" className="nav__link">
             <i className="fa fa-comments" /> <span className="hide-for-small">Blog</span>
-          </a>
+          </NavLink>
           <a href="#" className="nav__link">
             <i className="fa fa-camera" /> <span className="hide-for-small">Live session</span>
           </a>
@@ -69,6 +70,6 @@ export const Header = ({ openAskModal }) => (
 
     </div>
 
-    <div className="app-header__ghost"></div>
+    <div className="app-header__ghost" style={{height: '60px'}}></div>
   </header>
 );
